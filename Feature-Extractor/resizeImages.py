0200f13b1fe7eb11163a4c36622fcff44535597b
RayGonzalez <raygzz19@gmail.com>
2016-06-13 14:53:37 -0700

    Update resizeImages.py

--- Feature-Extractor/resizeImages.py
+++ Feature-Extractor/resizeImages.py
@@ -3,13 +3,13 @@ import scipy
 import os
 import numpy as np
 
-dir = '/home/ray/caffe/data/smallPlaces/smallPlaces'
+dir = '/home/ray/caffe/data/smallPlaces/smallPlaces/'
 
 for root, dirs, filenames in os.walk(dir):
     for f in filenames:
         path = dir + f
         Image = caffe.io.load_image(path)
         resized = caffe.io.resize_image(Image,(227,227))
-        newpath = '/home/ray/caffe/data/smallPlaces/resizedSmallPlaces'+f
-        np.save(newpath,resized)
+        newpath = '/home/ray/caffe/data/smallPlaces/resizedSmallPlaces/'+f
+        scipy.misc.imsave(newpath, resized)
 
